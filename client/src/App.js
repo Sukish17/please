e2a8513c12274af487ebd8c59282b98ed51a78e9
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-12 11:33:23 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-      axios.get('https://please-server.onrender.com/user', { withCredentials: true })
+      axios.get('https://please-server7.vercel.app/user', { withCredentials: true })
           .then(response => {
               if (response.data.user) {
                   setIsLoggedIn(true);
